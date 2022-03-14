e38ec03d49852fd40c5123eb403aa3fe3ecaac5c
Suraj Singh <surajsingh9090@gmail.com>
2022-03-15 01:09:47 +0530

    Logical Coding for marks after selecting answer is...DONE

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -8,34 +8,51 @@ import { QuestionService } from '../service/question.service';
 })
 export class QuestionComponent implements OnInit {
 
-  public name : string="";
-  public questionList : any = [];
-  public currentQuestion:number =0;
-  public points:number =0;
-  counter=60;
+  public name: string = "";
+  public questionList: any = [];
+  public currentQuestion: number = 0;
+  public points: number = 0;
+  counter = 60;
+  correctAnswer: number = 0;
+  inCorrectAnswer: number = 0;
 
-  constructor(private questionService : QuestionService) { }
+  constructor(private questionService: QuestionService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
   }
 
-  getAllQuestions(){
+  getAllQuestions() {
     this.questionService.getQuestionJson()
-    .subscribe(res=>{
-      //console.log(res.questions);
-      this.questionList = res.questions;
-    })
+      .subscribe(res => {
+        //console.log(res.questions);
+        this.questionList = res.questions;
+      })
 
   }
 
-  nextQuestion(){
+  nextQuestion() {
     this.currentQuestion++;
   }
 
-  previousQuestion(){
+  previousQuestion() {
     this.currentQuestion--;
+
+  }
+
+  answer(currentQno: number, option: any) {
+
+    if (option.correct) {
+      this.points += 10;
+      //this.points = this.points+10;
+      this.correctAnswer++;
+      this.currentQuestion++;
+    } else {
+      this.points -= 10;
+      this.currentQuestion++;
+      this.inCorrectAnswer++;
+    }
 
   }
 
